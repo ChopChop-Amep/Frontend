f9267ac169a7e740ced9ccd3f36e74a14e6a1cb9
PabloTutorMoegle <pablotutormoegle@gmail.com>
2025-03-12 20:12:00 +0100

    update

---
+++ Chop-Chop-WEB/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header className='header-style'>
+        <p style={{ color: '#000000', position: 'fixed', top: '0', left: '10px' }}> Search </p>
+      </header>
+      <h1> Chop Chop </h1>
+    </>
+  )
+}
+
+export default App
