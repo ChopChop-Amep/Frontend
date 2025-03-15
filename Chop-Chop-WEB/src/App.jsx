f042e79986873b3691e774e8a63d25407e8f68d5
PabloTutorMoegle <pablotutormoegle@gmail.com>
2025-03-15 21:27:36 +0100

    update

--- Chop-Chop-WEB/src/App.jsx
+++ Chop-Chop-WEB/src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
+import { useState } from 'react'
+import Login from './Components/LogIn.jsx'
+import Signup from './Components/SignUp.jsx'
 
 function App() {
+  const [showComponent, setShowComponent] = useState('login')
+
+  const handleShowLogin = () => setShowComponent('Login')
+  const handleShowSignup = () => setShowComponent('Signup')
 
   return (
     <>
       <header className='header-style'>
-        <p style={{ color: '#000000', position: 'fixed', top: '0', left: '10px' }}> Search </p>
-      </header>
-      <h1> Chop Chop </h1>
+        <input style={{ color: '#000000', position: 'fixed', top: '17px', left: '10px' }} placeholder='Search'/>
+        <button onClick={handleShowLogin} style={{ position: 'fixed', top: '17px', right: '10px' }}>Login</button>
+        <button onClick={handleShowSignup} style={{ position: 'fixed', top: '17px', right: '60px' }}>Sign Up</button>
+      </header> 
+      {showComponent === 'Login' ? <Login /> : <Signup />}
     </>
   )
 }
