393a82db9602c9703f6d0d1c2a5589e5ad7cde3e
PabloTutorMoegle <pablotutormoegle@gmail.com>
2025-03-19 13:05:24 +0100

    update

--- Chop-Chop-WEB/src/pages/HomePage.jsx
+++ Chop-Chop-WEB/src/pages/HomePage.jsx
@@ -29,10 +29,12 @@ function HomePage() {
   return (
     <>
       <header className='header-style'>
-        <input style={{ color: '#000000', position: 'fixed', top: '17px', left: '10px' }} placeholder='Search'/>
+        <input style={{backgroundColor: '#fff', color: '#000', position: 'fixed', top: '17px', left: '10px' }} placeholder='Search'/>
         <button onClick={handleShowLogin} className='button-style-hp' style={{ position: 'fixed', top: '17px', right: '10px' }}>Log In</button>
         <button onClick={handleShowSignup} className='button-style-hp' style={{ position: 'fixed', top: '17px', right: '100px' }}>Sign Up</button>
       </header>
+
+      <br />
 
       <main style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
         {products && products.map((product) => (
--- Chop-Chop-WEB/src/pages/utiles/Box.jsx
+++ Chop-Chop-WEB/src/pages/utiles/Box.jsx
@@ -4,7 +4,7 @@ import './Box.css';
 export function Box ({ title, image }) {
     return (
         <div className="box" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}>
-            <img src={image} alt={title} style={{ width: '150px' }} />
+            <img src={image} alt={title} style={{ width: '100px' }} />
             <h2 style={{ maxWidth: '150px', overflow: 'hidden', textOverflow: 'ellipsis', display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical' }}>{title}</h2>
         </div>
     );
