37d21f755d6b71785da7bdf3601ba07a356d130b
PabloTutorMoegle <pablotutormoegle@gmail.com>
2025-03-24 18:44:34 +0100

    update

--- Chop-Chop-WEB/src/pages/SignUp.jsx
+++ Chop-Chop-WEB/src/pages/SignUp.jsx
@@ -1,13 +1,7 @@
 import { useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 import './SignUp.css';
-
-// Initialize Supabase client
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; // Add to .env.local
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY; // Add to .env.local
-//const supabaseUrl = 'https://wldvmdxarlazwsswoywc.supabase.co'; // Replace <your_supabase_url>
-//const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsZHZtZHhhcmxhendzc3dveXdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxMzEyNDUsImV4cCI6MjA1NzcwNzI0NX0.j5lqbDl4mY1kIuNtUeCst_gRWvyDhFG4pRyB9oqLjHE'; // Replace <your_supabase_anon_key>
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+import supabase from './Authenticator.jsx';
 
 function SignUp() {
   const [name, setName] = useState('');
@@ -34,7 +28,7 @@ function SignUp() {
 
     if (name && surname && email && password) {
       try {
-        const { user, error } = await supabase.auth.signUp({
+        const { error } = await supabase.auth.signUp({
           email,
           password,
           options: {
@@ -62,41 +56,51 @@ function SignUp() {
     <main>
       <h1>Sign Up</h1>
       <form onSubmit={handleSubmit}>
+        <div>
           <input
             onChange={(e) => setName(e.target.value)}
             value={name}
             type="text"
             placeholder="Name"
           />
+        </div>
+        <div>
           <input
             onChange={(e) => setSurname(e.target.value)}
             value={surname}
             type="text"
             placeholder="Surname"
           />
+        </div>
+        <div>
           <input
             onChange={(e) => setEmail(e.target.value)}
             value={email}
             type="text"
             placeholder="Email"
           />
+        </div>
+        <div>
           <input
             onChange={(e) => setPassword(e.target.value)}
             value={password}
             type="password"
             placeholder="Password"
-          />        
-        <br />       
+          />
+        </div>
+        <br />
+        <div>
           <button className="button-style-su" type="submit">
             Sign Up
           </button>
+        </div>
       </form>
       {errorN && <p style={{ margin: '1rem', color: 'red' }}>{errorN}</p>}
       {errorS && <p style={{ margin: '1rem', color: 'red' }}>{errorS}</p>}
       {errorE && <p style={{ margin: '1rem', color: 'red' }}>{errorE}</p>}
       {errorP && <p style={{ margin: '1rem', color: 'red' }}>{errorP}</p>}
     </main>
-  );
+  )
 }
 
-export default SignUp;
+export default SignUp
