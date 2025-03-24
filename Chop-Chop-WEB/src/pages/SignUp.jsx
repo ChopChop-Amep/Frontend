bd336b6be552774fc67d3af5b5eec5496696fa43
PabloTutorMoegle <pablotutormoegle@gmail.com>
2025-03-24 17:51:04 +0100

    update

--- Chop-Chop-WEB/src/pages/SignUp.jsx
+++ Chop-Chop-WEB/src/pages/SignUp.jsx
@@ -3,8 +3,10 @@ import { createClient } from '@supabase/supabase-js';
 import './SignUp.css';
 
 // Initialize Supabase client
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; 
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; // Add to .env.local
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY; // Add to .env.local
+//const supabaseUrl = 'https://wldvmdxarlazwsswoywc.supabase.co'; // Replace <your_supabase_url>
+//const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsZHZtZHhhcmxhendzc3dveXdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxMzEyNDUsImV4cCI6MjA1NzcwNzI0NX0.j5lqbDl4mY1kIuNtUeCst_gRWvyDhFG4pRyB9oqLjHE'; // Replace <your_supabase_anon_key>
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 function SignUp() {
@@ -32,7 +34,7 @@ function SignUp() {
 
     if (name && surname && email && password) {
       try {
-        const { error } = await supabase.auth.signUp({
+        const { user, error } = await supabase.auth.signUp({
           email,
           password,
           options: {
@@ -83,16 +85,16 @@ function SignUp() {
             value={password}
             type="password"
             placeholder="Password"
-          />
-        <br />
+          />        
+        <br />       
           <button className="button-style-su" type="submit">
             Sign Up
           </button>
       </form>
-      <div>{errorN && <p style={{ margin: '1rem', color: 'red' }}>{errorN}</p>}</div>
-      <div>{errorS && <p style={{ margin: '1rem', color: 'red' }}>{errorS}</p>}</div>
-      <div>{errorE && <p style={{ margin: '1rem', color: 'red' }}>{errorE}</p>}</div>
-      <div>{errorP && <p style={{ margin: '1rem', color: 'red' }}>{errorP}</p>}</div>
+      {errorN && <p style={{ margin: '1rem', color: 'red' }}>{errorN}</p>}
+      {errorS && <p style={{ margin: '1rem', color: 'red' }}>{errorS}</p>}
+      {errorE && <p style={{ margin: '1rem', color: 'red' }}>{errorE}</p>}
+      {errorP && <p style={{ margin: '1rem', color: 'red' }}>{errorP}</p>}
     </main>
   );
 }
