bd042ac8b26ea90667bcd137a88de23a44c940b7
PabloTutorMoegle <pablotutormoegle@gmail.com>
2025-04-02 12:35:51 +0200

    update

--- Chop-Chop-WEB/src/pages/components/HeaderMenu.jsx
+++ Chop-Chop-WEB/src/pages/components/HeaderMenu.jsx
@@ -53,7 +53,7 @@ export function HeaderMenu() {
     if (event.key === 'Enter') {
       const query = event.target.value.trim();
       if (query) {
-        window.location.href = `/search/${encodeURIComponent(query)}`;
+        window.location.href = `/search?query=${encodeURIComponent(query)}`;
       }
     }
   };
