276ed4ddc1a3493dfe5f62d60e66c7e6a5ae4d45
PabloTutorMoegle <pablotutormoegle@gmail.com>
2025-04-07 21:23:55 +0200

    update log in conection to DataBase

--- Chop-Chop-WEB/src/pages/LogIn.jsx
+++ Chop-Chop-WEB/src/pages/LogIn.jsx
@@ -1,40 +1,71 @@
-import {useData} from '../hooks/useDataSignUp.js'
-import './LogIn.css'
+import { useState } from 'react';
+import { useData } from '../hooks/useDataSignUp.js';
+import './LogIn.css';
+import supabase from './Authenticator.jsx';
 
 function SignIn() {
-  const {email, updateEmail, errorE, password, updatePassword, errorP} = useData()
+  const { email, updateEmail, errorE, password, updatePassword, errorP } = useData();
+  const [loginError, setLoginError] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    if (!errorE && !errorP) {
-      alert('Signed In correctly.');
-      window.location.href = '/';
-    }
-  }
-  const handleChangeEmail = (event) => {
-    updateEmail(event.target.value)
-  }
-  const handleChangePassword = (event) => { 
-    updatePassword(event.target.value)
-  }
+    setLoginError(null);
 
+    if (!errorE && !errorP) {
+      try {
+        const { error } = await supabase.auth.signInWithPassword({
+          email,
+          password,
+        });
+
+        if (error) {
+          setLoginError('Invalid email or password.');
+        } else {
+          alert('Signed In correctly.');
+          window.location.href = '/';
+        }
+      } catch {
+        setLoginError('An error occurred while logging in.');
+      }
+    }
+  };
+
+  const handleChangeEmail = (event) => {
+    updateEmail(event.target.value);
+  };
+
+  const handleChangePassword = (event) => {
+    updatePassword(event.target.value);
+  };
+
+  const toggleShowPassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
 
   return (
     <main>
-        <h1>Log In</h1>
-        <form onSubmit={handleSubmit}>
-          <input className="inputEmail" onChange={handleChangeEmail} value={email} type="text" placeholder="Email" />
-          <br />
-          <input className="inputPassword" onChange={handleChangePassword} value={password} type="password" placeholder="Password" />
-          <br />
-          <br />
-          <button className='button-style-li' type='submit'>Log In</button>
-        </form>
-          {errorE && <p style={{margin: '1rem', color: 'red'}}>{errorE}</p>}
-          {errorP && <p style={{margin: '1rem', color: 'red'}}>{errorP}</p>}
-        
+      <h1>Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <input className="input-li" onChange={handleChangeEmail} value={email} type="text" placeholder="Email" />
+        <br />
+        <div style={{ position: 'relative', display: 'inline-block' }}>
+          <input className="input-li" onChange={handleChangePassword} value={password} type={showPassword ? 'text' : 'password'} placeholder="Password" />
+          <button type="button" onClick={toggleShowPassword} className="btn-show" >
+            {showPassword ? 'Hide' : 'Show'}
+          </button>
+        </div>
+        <br />
+        <br />
+        <button className="button-style-li" type="submit">
+          Log In
+        </button>
+      </form>
+      {errorE && <p style={{ margin: '1rem', color: 'red' }}>{errorE}</p>}
+      {errorP && <p style={{ margin: '1rem', color: 'red' }}>{errorP}</p>}
+      {loginError && <p style={{ margin: '1rem', color: 'red' }}>{loginError}</p>}
     </main>
-  )
+  );
 }
 
 export default SignIn
--- Chop-Chop-WEB/src/pages/components/HeaderMenu.jsx
+++ Chop-Chop-WEB/src/pages/components/HeaderMenu.jsx
@@ -1,53 +1,49 @@
 import React from 'react'
-import { useEffect, useState, useRef} from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './HeaderMenu.css'
 
 export function HeaderMenu() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Estado para verificar si el usuario está logueado
+  const dropdownRef = useRef(null);
 
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-    const dropdownRef = useRef(null);
-  
-    const DropDown = () => {
-      setIsDropdownOpen(!isDropdownOpen);
-    };
-  
-    const handleShowLogin = () => {
-      window.location.href = '/login';
-    };
-  
-    const handleShowSignup = () => {
-      window.location.href = '/signup';
-    };
+  const DropDown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
 
-    const handleShowCreateProduct = () => {
-      window.location.href = '/create-product'
-    };
+  const handleShowLogin = () => {
+    window.location.href = '/login';
+  };
 
-    const handleShowLogOut = () => {
-      if (window.confirm('Are you sure you want to log out?')) {
-        alert('You have logged out successfully!');
-        window.location.href = '/';
+  const handleShowSignup = () => {
+    window.location.href = '/signup';
+  };
+
+  const handleShowCreateProduct = () => {
+    window.location.href = '/create-product';
+  };
+
+  const handleShowLogOut = () => {
+    if (window.confirm('Are you sure you want to log out?')) {
+      alert('You have logged out successfully!');
+      setIsLoggedIn(false); // Cambiar el estado a no logueado
+      window.location.href = '/';
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsDropdownOpen(false);
       }
-
     };
-  
-  
-    useEffect(() => {
-      // Funció per tancar el desplegable si es fa clic fora
-      const handleClickOutside = (event) => {
-        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-          setIsDropdownOpen(false);
-        }
-      };
-  
-      // Afegir l'escoltador d'esdeveniments al document
-      document.addEventListener('click', handleClickOutside);
-  
-      // Netejar l'escoltador quan el component es desmunta
-      return () => {
-        document.removeEventListener('click', handleClickOutside);
-      };
-    }, []);
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
   const handleSearch = (event) => {
     if (event.key === 'Enter') {
@@ -66,9 +62,10 @@ export function HeaderMenu() {
         onKeyDown={handleSearch}
       />
 
-      <img src='../../public/logo_processed.png' 
-        style={{ 
-          maxWidth: '40px',  
+      <img
+        src='../../public/logo_processed.png'
+        style={{
+          maxWidth: '40px',
           cursor: 'pointer',
           borderRadius: '10%',
           position: 'fixed',
@@ -76,59 +73,73 @@ export function HeaderMenu() {
         }}
         onClick={() => {
           window.location.href = '/';
-        }}/>
+        }}
+      />
 
       <img
-          src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
-          alt="Imatge User"
-          width="40"
-          height="40"
-          style={{
+        src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+        alt="Imatge User"
+        width="40"
+        height="40"
+        style={{
           cursor: 'pointer',
           borderRadius: '50%',
           position: 'fixed',
           top: '10px',
-          right: '10px'
-          }}
-          onClick={(e) => {
-          e.stopPropagation(); // Evita que el clic a la imatge tanqui immediatament el desplegable
+          right: '10px',
+        }}
+        onClick={(e) => {
+          e.stopPropagation();
           DropDown();
-          }}
+        }}
       />
 
       {isDropdownOpen && (
         <div
-          ref={dropdownRef} // Referència per detectar clics fora
+          ref={dropdownRef}
           style={{
-              position: 'fixed',
-              top: '55px',
-              right: '10px',
-              background: 'white',
-              border: '1px solid black',
-              padding: '10px',
-              borderRadius: '5px',
-              boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
-              zIndex: 1000
+            position: 'fixed',
+            top: '55px',
+            right: '10px',
+            background: 'white',
+            border: '1px solid black',
+            padding: '10px',
+            borderRadius: '5px',
+            boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+            zIndex: 1000,
           }}
-          >
-          <a href="/login" onClick={handleShowLogin} style={{ color: '#000', display: 'block', padding: '5px' }}>
-              Log In
-          </a>
-          <br />
-          <a href="/signup" onClick={handleShowSignup} style={{ color: '#000', display: 'block', padding: '5px' }}>
-              Sign Up
-          </a>
-          <br />
-          <a href="/create-product" onClick={handleShowCreateProduct} style={{ color: '#000', display: 'block', padding: '5px' }}>
-              Add Product
-          </a>
-          <br />
-          <a href="/" onClick={handleShowLogOut} style={{ color: '#000', display: 'block', padding: '5px' }}>
-              Log Out
-          </a>
+        >
+          {!isLoggedIn ? (
+            <>
+              <a href="/login" onClick={handleShowLogin} style={{ color: '#000', display: 'block', padding: '5px' }}>
+                Log In
+              </a>
+              <br />
+              <a href="/signup" onClick={handleShowSignup} style={{ color: '#000', display: 'block', padding: '5px' }}>
+                Sign Up
+              </a>
+              <br />
+              <a href="/create-product" onClick={handleShowCreateProduct} style={{ color: '#000', display: 'block', padding: '5px' }}>
+                Add Product
+              </a>
+              <br />
+              <a href="/" onClick={handleShowLogOut} style={{ color: '#000', display: 'block', padding: '5px' }}>
+                Log Out
+              </a>
+            </>
+          ) : (
+            <>
+              <a href="/create-product" onClick={handleShowCreateProduct} style={{ color: '#000', display: 'block', padding: '5px' }}>
+                Add Product
+              </a>
+              <br />
+              <a href="/" onClick={handleShowLogOut} style={{ color: '#000', display: 'block', padding: '5px' }}>
+                Log Out
+              </a>
+            </>
+          )}
         </div>
       )}
     </header>
-  )
+  );
 }
-
