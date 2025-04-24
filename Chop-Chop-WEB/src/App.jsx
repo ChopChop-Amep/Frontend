40085498f8ede0f6f401e8160a9eddb6a0497498
claraylv4 <claraylv4@gmail.com>
2025-04-24 18:05:36 +0200

    Pagina de Factura d'una compra d'un o més productes

--- Chop-Chop-WEB/src/App.jsx
+++ Chop-Chop-WEB/src/App.jsx
@@ -4,7 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
 import Login from './pages/LogIn.jsx'
 import Signup from './pages/SignUp.jsx'
-
+import Profile from './pages/Profile.jsx'
+import Invoice from './pages/Invoice.jsx'
 function App() {
 
   return (
@@ -13,6 +14,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Invoice" element ={<Invoice />} />
       </Routes>
     </Router>
   )
