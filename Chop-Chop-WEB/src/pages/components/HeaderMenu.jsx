d2a5df2f86b804dedf10cad22af4275acf29cca1
PabloTutorMoegle <pablotutormoegle@gmail.com>
2025-04-28 13:12:49 +0200

    update

--- Chop-Chop-WEB/src/pages/components/HeaderMenu.jsx
+++ Chop-Chop-WEB/src/pages/components/HeaderMenu.jsx
@@ -8,6 +8,7 @@ export function HeaderMenu() {
   const dropdownRef = useRef(null);
 
   const token = localStorage.getItem('authToken'); // Cambia 'authToken' por el nombre de tu token
+  console.log(token ? token : 'no token' )
   const decodedToken = token ? JSON.parse(atob(token.split('.')[1])) : null; // Decodifica el token JWT
 
 // user_metadata: 
@@ -87,53 +88,53 @@ export function HeaderMenu() {
         src='/logo_processed.png'
         onClick={() => {
             window.location.href = '/';
-            }}
-            />
+        }}
+      />
 
-            <select
-            className="select-header"
-            onChange={(e) => {
-              const selectedCategory = e.target.value;
-              if (selectedCategory !== "all") {
-              window.location.href = `/search?query=${encodeURIComponent(selectedCategory)}`;
-              }
-            }}
-            >
-            <option value="all">Category</option>
-            <option value="artesanal">Artesanal</option>
-            <option value="antiguitats">Antiguitats</option>
-            <option value="cosmetica">Cosmètica</option>
-            <option value="cuina">Cuina</option>
-            <option value="electrodomestics">Electrodomèstics</option>
-            <option value="electronica">Electrònica</option>
-            <option value="equipament_lab">Equipament de Laboratori</option>
-            <option value="esports">Esports</option>
-            <option value="ferramentes">Ferramentes</option>
-            <option value="infantil">Infantil</option>
-            <option value="instruments">Instruments</option>
-            <option value="jardineria">Jardineria</option>
-            <option value="jocs_de_taula">Jocs de Taula</option>
-            <option value="joies_complements_accessoris">Joies, Complements i Accessoris</option>
-            <option value="llibres">Llibres</option>
-            <option value="mascotes">Mascotes</option>
-            <option value="mobles">Mobles</option>
-            <option value="neteja">Neteja</option>
-            <option value="roba">Roba</option>
-            <option value="sabates">Sabates</option>
-            <option value="vehicles">Vehicles</option>
-            <option value="videojocs">Videojocs</option>
-            </select>
+      <select
+      className="select-header"
+      onChange={(e) => {
+        const selectedCategory = e.target.value;
+        if (selectedCategory !== "all") {
+        window.location.href = `/search?query=${encodeURIComponent(selectedCategory)}`;
+        }
+      }}
+      >
+        <option value="all">Category</option>
+        <option value="artesanal">Artesanal</option>
+        <option value="antiguitats">Antiguitats</option>
+        <option value="cosmetica">Cosmètica</option>
+        <option value="cuina">Cuina</option>
+        <option value="electrodomestics">Electrodomèstics</option>
+        <option value="electronica">Electrònica</option>
+        <option value="equipament_lab">Equipament de Laboratori</option>
+        <option value="esports">Esports</option>
+        <option value="ferramentes">Ferramentes</option>
+        <option value="infantil">Infantil</option>
+        <option value="instruments">Instruments</option>
+        <option value="jardineria">Jardineria</option>
+        <option value="jocs_de_taula">Jocs de Taula</option>
+        <option value="joies_complements_accessoris">Joies, Complements i Accessoris</option>
+        <option value="llibres">Llibres</option>
+        <option value="mascotes">Mascotes</option>
+        <option value="mobles">Mobles</option>
+        <option value="neteja">Neteja</option>
+        <option value="roba">Roba</option>
+        <option value="sabates">Sabates</option>
+        <option value="vehicles">Vehicles</option>
+        <option value="videojocs">Videojocs</option>
+      </select>
 
-            {isLoggedIn && (
-            <a style = {{color: '#000', position: 'fixed', top: '18px', right: '70px'}}> {decodedToken.user_metadata.name} </a>
-            )}
-            <img
-            src={(decodedToken.user_metadata?.image) || 'https://cdn-icons-png.flaticon.com/512/149/149071.png'}
-            alt="Imatge User"
-            width="40"
-            height="40"
-            className='img-user'
-            onClick={(e) => {
+      {isLoggedIn && (
+        <a style = {{color: '#000', position: 'fixed', top: '18px', right: '70px'}}> {decodedToken.user_metadata.name} </a>
+      )}
+      <img
+        src={(decodedToken ? decodedToken.user_metadata?.image : 'https://cdn-icons-png.flaticon.com/512/149/149071.png')}
+        alt="Imatge User"
+        width="40"
+        height="40"
+        className='img-user'
+        onClick={(e) => {
           e.stopPropagation();
           DropDown();
         }}
